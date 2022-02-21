f5d0163c56720fde0026083e45f18909d289888c
ThangVu0609 <vuducthang025@gmail.com>
2022-02-21 22:00:50 +0900

    cập nhật phần xóa/ sửa

--- day_15_review_javascript/javascript.js
+++ day_15_review_javascript/javascript.js
@@ -1,7 +1,8 @@
-var name = "....";
-var birthday = "....";
-var sex = "....";
-var address = "....";
+var name = "...";
+var birthday = "...";
+var sex = "...";
+var address = "...";
+
 // thay thế HTML chứa các đoạn mã input nhập thông tin
 function replaceHTML(id, value) {
     document.getElementById(id).innerHTML = value;
@@ -10,15 +11,21 @@ function replaceHTML(id, value) {
 function makeTagInput(id, value, type) {
     return "<input id=\"" + id + "\" type=\"" + type + "\" value=\"" + value + "\">";
 }
+function makeTagTextarea(id, value) {
+    return "<textarea id=\"" + id + "\">" + value + "</textarea>";
+}
+
 // Tạo các mã HTML chứa action link
 function makeActionLink(event, label) {
     return "<a href='javascript:;' onclick='" + event +  "()'>" + label + "</a>";
 }
+
 function profileOnEdit() {
-    var cancelLink = makeActionLink("profileOnCancel", "Hủy");
+    var cancelLink = makeActionLink("profileOnCancel", "Huỷ");
     var updateLink = makeActionLink("profileOnUpdate", "Cập nhật");
-    //// thay thế HTML chứa đoạn mã các link Huỷ và Cập nhật
-    document.getElementById("profileActionButtons").innerHTML = cancelLink + updateLink;
+
+    // thay thế HTML chứa đoạn mã các link Huỷ và Cập nhật
+    document.getElementById("profileActionButtons").innerHTML = cancelLink + " " + updateLink;
 
     // thay thế HTML chứa các đoạn mã input nhập thông tin
     // replaceHTML("name", `<input id="nameInput" type="text" value="${name}">`); // ES6 Literal Template String
@@ -27,17 +34,7 @@ function profileOnEdit() {
     replaceHTML("sex", makeTagInput("sexInput", sex, "text")); 
     replaceHTML("address", makeTagInput("addressInput", address, "text"));
 }
-function profileOnCancel() {
-    // lấy giá trị cũ để hiển thị ra HTML
-    replaceHTML("name", name);
-    replaceHTML("birthday", birthday);
-    replaceHTML("sex", sex);
-    replaceHTML("address", address);
 
-    // hiển thị link Sửa
-    var editLink = makeActionLink("profileOnEdit", "Sửa");
-    document.getElementById("profileActionButtons").innerHTML = editLink;
-}
 function profileOnUpdate() {
     // lấy giá trị đã nhập và cập nhật vào các biến toàn cục
     name = document.getElementById("nameInput").value;
@@ -56,28 +53,132 @@ function profileOnUpdate() {
     document.getElementById("profileActionButtons").innerHTML = editLink;
 }
 
+function profileOnCancel() {
+    // lấy giá trị cũ để hiển thị ra HTML
+    replaceHTML("name", name);
+    replaceHTML("birthday", birthday);
+    replaceHTML("sex", sex);
+    replaceHTML("address", address);
 
-
-// BÀI LÀM CHO THỨ 2
-
-var numberOfRow = 2;
-//function contentOnEdit() {
-
-//}
-// Xóa dòng trong bảng
-function deleteRow(row) {
-    //var i=row.parentNode.parentNode.parentNode.parentNode.rowIndex;
-    //console.log(row);
-    document.getElementById("tBody").deleteRow(row);
-    numberOfRow = document.getElementById("tBody").childNodes;
-    for(let i = row; i < numberOfRow; i++){
-        document.getElementById("tBody").getElementsByClassName("STT")[row].innerHTML = row;
-    }
-    //var kh = document.getElementById("tBody");
-    //kh.removeChild(kh.childNodes[i]);
+    // hiển thị link Sửa
+    var editLink = makeActionLink("profileOnEdit", "Sửa");
+    document.getElementById("profileActionButtons").innerHTML = editLink;
 }
 
-// Thêm dòng trong bảng
-//function addRow() {
+// Object to store a list of workingExperience
+var workingExperience = [
+    {
+        "time": "05/2014 - 7/2020",
+        "companyName": "VNG",
+        "position": "Software Engineer",
+        "job": '<ul class="list-with-image">\
+                    <li>Design</li>\
+                    <li>Develop</li>\
+                </ul>'
+    },
+    {
+        "time": "07/2020 - Current",
+        "companyName": "Vsee",
+        "position": "Software Engineer",
+        "job": '<ul class="list-with-image">\
+                    <li>Design</li>\
+                    <li>Develop</li>\
+                </ul>'
+    }
+];
 
-//}
+
+// Tạo các mã HTML chứa action link
+function makeActionLinkForEachRowOfWorkingExp(event, label, index) {
+    return "<a href='javascript:;' onclick='" + event +  "(" + index + ")'>" + label + "</a>";
+}
+
+function makeTableBodyOfWorkingExp() {
+    var html = "";
+
+    workingExperience.forEach(function(item, index) {
+        var editLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnEdit", "Sửa", index);
+        var deleteLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnDelete", "Xoá", index);
+
+        html += '<tr>\
+                    <td>' 
+                        + (index + 1) + '<br/>' 
+                        + '<span id="expActions_' + index + '">' + editLink + '<br/>' + deleteLink + '</span>' +
+                    '</td>\
+                    <td id="expTime_' + index + '">' + item.time + '</td>\
+                    <td id="expCompanyName_' + index + '">' + item.companyName + '</td>\
+                    <td id="expPosition_' + index + '">' + item.position + '</td>\
+                    <td id="expJob_' + index + '" align="left">' + item.job + '</td>\
+                </tr>';
+    });
+
+    replaceHTML("tableWorkingExp", html);
+}
+
+makeTableBodyOfWorkingExp();
+
+function workingExpOnEdit(index) {
+    var updateLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnUpdate", "Cập nhật", index);
+    var cancelLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnCancel", "Hủy", index);
+    replaceHTML("expActions_" + index,updateLink + '<br/>' + cancelLink)
+    replaceHTML(
+        "expTime_" + index, 
+        makeTagInput("expTimeInput_" + index, workingExperience[index].time, "text")
+    );
+
+    replaceHTML(
+        "expCompanyName_" + index, 
+        makeTagInput("expCompanyNameInput_" + index, workingExperience[index].companyName, "text")
+    );
+
+    replaceHTML(
+        "expPosition_" + index, 
+        makeTagInput("expPositionInput_" + index, workingExperience[index].position, "text")
+    );
+
+    replaceHTML(
+        "expJob_" + index, 
+        makeTagTextarea("expJobTextarea_" + index, workingExperience[index].job)
+    );
+}
+
+function workingExpOnDelete(index) {
+    if (confirm("Bạn có muốn xoá dòng " + (index + 1)) == true) {
+        workingExperience.splice(index, 1);
+        makeTableBodyOfWorkingExp();
+    } else {
+        //
+    }
+}
+
+// In gia tri cua mot Item trong Object ra dong HTML tuong ung
+function printItemValue(index) {
+    var editLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnEdit", "Sửa", index);
+    var deleteLink = makeActionLinkForEachRowOfWorkingExp("workingExpOnDelete", "Xoá", index);
+    replaceHTML("expActions_" + index,editLink + '<br/>' + deleteLink);
+    replaceHTML("expTime_" + index,workingExperience[index].time );
+
+    replaceHTML(
+        "expCompanyName_" + index,workingExperience[index].companyName
+    );
+
+    replaceHTML(
+        "expPosition_" + index,workingExperience[index].position
+    );
+
+    replaceHTML(
+        "expJob_" + index,workingExperience[index].job
+    );
+    
+}
+function workingExpOnUpdate(index) {
+    workingExperience[index].time = document.getElementById("expTimeInput_" + index).value;
+    workingExperience[index].companyName = document.getElementById("expCompanyNameInput_" + index).value;
+    workingExperience[index].position = document.getElementById("expPositionInput_" + index).value;
+    workingExperience[index].job = document.getElementById("expJobTextarea_" + index).value;
+    printItemValue(index);
+}
+
+function workingExpOnCancel(index) {
+    printItemValue(index);
+}
